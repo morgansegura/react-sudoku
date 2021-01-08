6f9b0c7171ee0b8809223d0a1ce9533433ea236e
Morgan Segura <morgansegura@gmail.com>
2021-01-08 15:41:38 -0800

    Adds in row and cols for sudoku map. Adds props for cols and rows in block component

---
+++ src/components/grid/index.tsx
@@ -0,0 +1,27 @@
+import React, { FC, Children } from 'react'
+import { Container, Row } from './styles'
+import Block from './block'
+
+const Grid: FC = () => {
+	// other stuff
+	return (
+		<Container data-cy='grid-container'>
+			{Children.toArray(
+				[...Array(9)].map((_, rowIndex) => (
+					<Row data-cy='grid-row-container'>
+						{Children.toArray(
+							[...Array(9)].map((_, colIndex) => (
+								<Block
+									colIndex={colIndex}
+									rowIndex={rowIndex}
+								/>
+							))
+						)}
+					</Row>
+				))
+			)}
+		</Container>
+	)
+}
+
+export default Grid
