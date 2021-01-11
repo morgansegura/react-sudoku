f0ffa5efbc3e7038cced9915f466ad753ffbc314
Morgan Segura <morgansegura@gmail.com>
2021-01-11 14:52:46 -0800

    Persists state of game in browser cache and adds new game button

--- src/components/numbers/button/index.tsx
+++ src/components/numbers/button/index.tsx
@@ -1,17 +1,38 @@
-import React, { FC } from 'react'
+import React, { FC, useCallback } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { AnyAction, Dispatch } from 'redux'
 
 import { Button } from 'components'
+import { fillBlock, IReducer } from 'reducers'
 
-import { NUMBERS } from 'typings'
+import { NUMBERS, BLOCK_COORDS, N } from 'typings'
 
 interface IProps {
 	value: NUMBERS
 }
+interface IState {
+	selectedBlock?: BLOCK_COORDS
+	selectedValue?: N
+}
 
 const NumberButton: FC<IProps> = ({ value }) => {
-	function fill() {
-		console.log('fill', value)
-	}
+	const state = useSelector<IReducer, IState>(
+		({ selectedBlock, workingGrid }) => ({
+			selectedBlock,
+			selectedValue:
+				workingGrid && selectedBlock
+					? workingGrid[selectedBlock[0]][selectedBlock[1]]
+					: 0,
+		})
+	)
+	const dispatch = useDispatch<Dispatch<AnyAction>>()
+
+	const fill = useCallback(() => {
+		if (state.selectedBlock && state.selectedValue === 0) {
+			dispatch(fillBlock(value, state.selectedBlock))
+		}
+	}, [dispatch, state.selectedBlock, state.selectedValue, value])
+
 	return <Button onClick={fill}>{value}</Button>
 }
 
